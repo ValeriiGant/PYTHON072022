7889a8777a21ebdd076068b0e71c28982fd0f841
Valerii GANTCEV <4vpost@gmail.com>
2022-08-27 00:38:11 +0500

    Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.

--- HomeWork/H6W/H6W1.py
+++ HomeWork/H6W/H6W1.py
@@ -1,7 +1,16 @@
-Transformation = lambda x: x*1
-val = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
-transformed_val = list(map(Transformation, val))
-if val == transformed_val:
-    print('OK')
-else:
-    print('no')
+# 3. Задайте список из вещественных чисел. Напишите программу, которая найдёт
+# разницу между максимальным и минимальным значением дробной части элементов.
+
+#     *Пример:*
+
+#     - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+def fraction_part_list(list_my: list) -> list:
+    return [round(i % 1, 2) for i in list_my if type(i) == float]
+
+
+new_list = [1.1, 1.2, 3.1, 5, 10.01]
+print("Initial list: ", new_list)
+new_list_my = fraction_part_list(new_list)
+print("Fractional parts list: ", new_list_my)
+print("Difference between man and min fractional part =", max(new_list_my) - min(new_list_my))
